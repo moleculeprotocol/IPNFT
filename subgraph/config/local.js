5bfc63097fcbf83b930a748df361d835e017a0e6
Stefan Adolf <stefan@molecule.to>
2022-10-27 12:41:51 +0200

    reuses main repo's .env file for subgraph config
    Signed-off-by: Stefan Adolf <stefan@molecule.to>

--- subgraph/config/local.js
+++ subgraph/config/local.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
+const path = require("node:path");
+require('dotenv').config({
+    debug: true,
+    path: path.resolve(process.cwd(), '../.env')
+})
 
 module.exports = {
     network: 'mainnet',
     ipnft: {
-        address: process.env.ANVIL_IPNFT_CONTRACT_ADDRESS,
+        address: process.env.IPNFT_ADDRESS,
         startBlock: 0,
     },
     simpleOpenSea: {
-        address: process.env.ANVIL_SIMPLEOPENSEA_CONTRACT_ADDRESS,
+        address: process.env.SOS_ADDRESS,
         startBlock: 0,
     },
 };
