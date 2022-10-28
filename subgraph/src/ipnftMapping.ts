80f5f1a4c86969eb79d2a7714cad9097ea937341
Stefan Adolf <stefan@molecule.to>
2022-10-28 16:03:41 +0200

    splits mapping to 2 files
    Signed-off-by: Stefan Adolf <stefan@molecule.to>

---
+++ subgraph/src/ipnftMapping.ts
@@ -0,0 +1,50 @@
+import { Address, log, store } from "@graphprotocol/graph-ts";
+import {
+    ReservationURIUpdated as ReservationURIUpdatedEvent,
+    Reserved as ReservedEvent,
+    TokenMinted as TokenMintedEvent,
+    TransferSingle
+} from "../generated/IPNFT/IPNFT";
+import { Ipnft, Reservation } from "../generated/schema";
+
+export function handleTransferSingle(event: TransferSingle): void {
+    if (event.params.from !== Address.zero()) {
+        let ipnft = Ipnft.load(event.params.id.toString());
+        if (ipnft) {
+            ipnft.owner = event.params.to;
+            ipnft.save();
+        }
+    }
+}
+
+export function handleReservation(event: ReservedEvent): void {
+    let reservation = new Reservation(event.params.reservationId.toString());
+    reservation.owner = event.params.reserver;
+    reservation.createdAt = event.block.timestamp;
+    reservation.save();
+}
+
+export function handleReservationURIUpdated(
+    event: ReservationURIUpdatedEvent
+): void {
+    const reservation = Reservation.load(event.params.reservationId.toString());
+    if (!reservation) {
+        log.debug(
+            `could not load reservation from reservationId: ${event.params.reservationId.toString()}`,
+            []
+        );
+    } else {
+        reservation.uri = event.params.tokenURI;
+        reservation.save();
+    }
+}
+
+export function handleMint(event: TokenMintedEvent): void {
+    let ipnft = new Ipnft(event.params.tokenId.toString());
+    ipnft.owner = event.params.owner;
+    ipnft.createdAt = event.block.timestamp;
+    ipnft.tokenURI = event.params.tokenURI;
+    ipnft.save();
+
+    store.remove("Reservation", event.params.tokenId.toString());
+}
--- subgraph/src/swapMapping.ts
+++ subgraph/src/swapMapping.ts
@@ -1,56 +1,17 @@
-import { Address, Bytes, log, store } from '@graphprotocol/graph-ts';
+import { Bytes, log } from "@graphprotocol/graph-ts";
+import { Ipnft, Listing } from "../generated/schema";
 import {
-    IPNFT,
-    TransferSingle as TransferSingleEvent,
-    Reserved as ReservedEvent,
-    TokenMinted as TokenMintedEvent,
-    ReservationURIUpdated as ReservationURIUpdatedEvent,
-} from '../generated/IPNFT/IPNFT';
-import {
-    Listed as ListedEvent,
-    Unlisted as UnlistedEvent,
-    Purchased as PurchasedEvent,
     AllowlistUpdated as AllowlistUpdatedEvent,
-} from '../generated/SchmackoSwap/SchmackoSwap';
-import { Ipnft, Listing, Reservation } from '../generated/schema';
-
-export function handleReservation(event: ReservedEvent): void {
-    let reservation = new Reservation(event.params.reservationId.toString());
-    reservation.owner = event.params.reserver;
-    reservation.createdAt = event.block.timestamp;
-    reservation.save();
-}
-
-export function handleReservationURIUpdate(
-    event: ReservationURIUpdatedEvent
-): void {
-    const reservation = Reservation.load(event.params.reservationId.toString());
-    if (!reservation) {
-        log.debug(
-            `could not load reservation from reservationId: ${event.params.reservationId.toString()}`,
-            []
-        );
-    } else {
-        reservation.uri = event.params.tokenURI;
-        reservation.save();
-    }
-}
-
-export function handleMint(event: TokenMintedEvent): void {
-    let ipnft = new Ipnft(event.params.tokenId.toString());
-    ipnft.owner = event.params.owner;
-    ipnft.createdAt = event.block.timestamp;
-    ipnft.tokenURI = event.params.tokenURI;
-    ipnft.save();
-
-    store.remove('Reservation', event.params.tokenId.toString());
-}
+    Listed as ListedEvent,
+    Purchased as PurchasedEvent,
+    Unlisted as UnlistedEvent
+} from "../generated/SchmackoSwap/SchmackoSwap";
 
 export function handleListed(event: ListedEvent): void {
     let listing = new Listing(event.params.listingId.toString());
     let ipnft = Ipnft.load(event.params.listing.tokenId.toString());
     if (!ipnft) {
-        log.debug('Could not load ipnft from tokenId', []);
+        log.debug("Could not load ipnft from tokenId", []);
     } else {
         listing.ipnft = ipnft.id;
     }
@@ -117,18 +78,4 @@ export function handlePurchased(event: PurchasedEvent): void {
     listing.purchasedAt = event.block.timestamp;
     listing.buyer = event.params.buyer;
     listing.save();
-}
-
-export function handleTransferSingle(event: TransferSingleEvent): void {
-    // Do not handle Mints as they are handled by the handleMint function
-    if (
-        event.params.from !==
-        Address.fromString('0x0000000000000000000000000000000000000000')
-    ) {
-        let ipnft = Ipnft.load(event.params.id.toString());
-        if (ipnft) {
-            ipnft.owner = event.params.to;
-            ipnft.save();
-        }
-    }
 }
