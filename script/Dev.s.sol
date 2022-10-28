f7efa85bd6222da7e172cedbaa0f00161022b474
Stefan Adolf <stefan@molecule.to>
2022-10-28 16:29:12 +0200

    splits mapping, dedicated and deployment deployment scripts (#11)
    * splits mapping to 2 files
    * dev deployment script
    * regenerated abis / compound command
    * individual deployment scripts

--- script/Dev.s.sol
+++ script/Dev.s.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
+import "../src/MyToken.sol";
 import "../src/IPNFT.sol";
+import "../src/SchmackoSwap.sol";
 
-contract IPNFTScript is Script {
+contract DevScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
         new IPNFT();
+        new SchmackoSwap();
+        new MyToken();
         vm.stopBroadcast();
     }
 }
