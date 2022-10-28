a6fba90cae707f7646d6667fa1dd12be7f19bc34
Dorian Wilhelm <wilhelm.dorian@pm.me>
2022-10-28 15:43:11 +0200

    creates unified script

--- script/IPNFT.s.sol
+++ script/IPNFT.s.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
+import "../src/MyToken.sol";
 import "../src/IPNFT.sol";
+import "../src/SchmackoSwap.sol";
 
 contract IPNFTScript is Script {
     function setUp() public {}
@@ -11,6 +13,8 @@ contract IPNFTScript is Script {
     function run() public {
         vm.startBroadcast();
         new IPNFT();
+        new SchmackoSwap();
+        new MyToken();
         vm.stopBroadcast();
     }
 }
