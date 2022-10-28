11fae377908b18dbd45a2b516e7ca5c264d1e54b
Dorian Wilhelm <wilhelm.dorian@pm.me>
2022-10-28 10:44:27 +0200

    deletes IPNFT ERC721 and replaces with ERC1155

--- src/IPNFT.sol
+++ src/IPNFT.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+import {ERC1155URIStorage} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+
+/*
+ ______ _______         __    __ ________ ________
+|      \       \       |  \  |  \        \        \
+ \▓▓▓▓▓▓ ▓▓▓▓▓▓▓\      | ▓▓\ | ▓▓ ▓▓▓▓▓▓▓▓\▓▓▓▓▓▓▓▓
+  | ▓▓ | ▓▓__/ ▓▓______| ▓▓▓\| ▓▓ ▓▓__      | ▓▓
+  | ▓▓ | ▓▓    ▓▓      \ ▓▓▓▓\ ▓▓ ▓▓  \     | ▓▓
+  | ▓▓ | ▓▓▓▓▓▓▓ \▓▓▓▓▓▓ ▓▓\▓▓ ▓▓ ▓▓▓▓▓     | ▓▓
+ _| ▓▓_| ▓▓            | ▓▓ \▓▓▓▓ ▓▓        | ▓▓
+|   ▓▓ \ ▓▓            | ▓▓  \▓▓▓ ▓▓        | ▓▓
+ \▓▓▓▓▓▓\▓▓             \▓▓   \▓▓\▓▓         \▓▓
+
+*/
 
 contract IPNFT is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    Pausable,
+    ERC1155,
     Ownable,
-    ERC721Burnable
+    Pausable,
+    ERC1155Burnable,
+    ERC1155Supply,
+    ERC1155URIStorage
 {
     using Counters for Counters.Counter;
 
@@ -26,14 +39,11 @@ contract IPNFT is
         string tokenURI;
     }
 
-    uint256 private _price = 0 ether;
+    uint256 public mintPrice = 0 ether;
 
     mapping(uint256 => Reservation) public reservations;
 
-    event Reserved(
-        address indexed reserver,
-        uint256 indexed reservationId
-    );
+    event Reserved(address indexed reserver, uint256 indexed reservationId);
     event ReservationURIUpdated(
         string tokenURI,
         address indexed reserver,
@@ -42,15 +52,14 @@ contract IPNFT is
     event TokenMinted(
         string tokenURI,
         address indexed owner,
-        uint256 indexed tokenId
+        uint256 indexed tokenId,
+        uint256 shares
     );
 
     /// @dev https://docs.opensea.io/docs/metadata-standards#freezing-metadata
     event PermanentURI(string _value, uint256 indexed _id);
 
-    constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol)
-    {}
+    constructor() ERC1155("") {}
 
     function pause() public onlyOwner {
         _pause();
@@ -60,8 +69,36 @@ contract IPNFT is
         _unpause();
     }
 
-    function updatePrice(uint256 amount) public onlyOwner {
-        _price = amount;
+    function updateMintPrice(uint256 amount) public onlyOwner {
+        mintPrice = amount;
+    }
+
+    // This is how a direct mint could look like for IP-NFTs that don't need encrypted legal contracts.
+    // The Question is how useful this is, after all the legal contract might still require
+    // a hard reference to the IP-NFT tokenId. For this function you wouldn't get that
+    // tokenId until after the mint of course.
+    function directMint(address to, string memory tokenURI)
+        public
+        payable
+        returns (uint256 tokenId)
+    {
+        require(msg.value >= mintPrice, "Ether amount sent is too small");
+
+        uint256 newTokenId = _reservationCounter.current();
+        _reservationCounter.increment();
+
+        // Given that we're not super confident about the metadata being "final" yet,
+        // I don't think we should set the permanent URI yet.
+        emit PermanentURI(tokenURI, newTokenId);
+
+        emit TokenMinted(tokenURI, to, newTokenId, 1);
+
+
+
+        _mint(to, newTokenId, 1, "");
+        _setURI(newTokenId, tokenURI);
+
+        return tokenId;
     }
 
     function mintReservation(address to, uint256 reservationId)
@@ -69,23 +106,35 @@ contract IPNFT is
         payable
         returns (uint256 tokenId)
     {
-        require(msg.value == _price, "Ether amount sent is not correct");
+        return
+            mintReservation(
+                to,
+                reservationId,
+                reservations[reservationId].tokenURI
+            );
+    }
+
+    function mintReservation(
+        address to,
+        uint256 reservationId,
+        string memory tokenURI
+    ) public payable returns (uint256 tokenId) {
+        require(msg.value >= mintPrice, "Ether amount sent is too small");
         require(
             reservations[reservationId].reserver == _msgSender(),
-            "IP NFT: caller is not reserver"
+            "IP-NFT: caller is not reserver"
         );
 
-        _safeMint(to, reservationId);
-        _setTokenURI(reservationId, reservations[reservationId].tokenURI);
+        // Given that we're not super confident about the metadata being "final" yet,
+        // I don't think we should set the permanent URI yet.
+        emit PermanentURI(tokenURI, reservationId);
 
-        emit PermanentURI(reservations[reservationId].tokenURI, reservationId);
-        emit TokenMinted(
-            reservations[reservationId].tokenURI,
-            to,
-            reservationId
-        );
+        emit TokenMinted(tokenURI, to, reservationId, 1);
 
         delete reservations[reservationId];
+
+        _mint(to, reservationId, 1, "");
+        _setURI(reservationId, tokenURI);
 
         return reservationId;
     }
@@ -101,55 +150,59 @@ contract IPNFT is
         return reservationId;
     }
 
-    function updateReservationURI(uint256 reservationId, string calldata _tokenURI)
-        external
-    {
+    function updateReservationURI(
+        uint256 reservationId,
+        string calldata _tokenURI
+    ) external {
         require(
             reservations[reservationId].reserver == _msgSender(),
-            "Reservation not valid or not owned by you"
+            "IP-NFT: Reservation not valid or not owned by you"
         );
 
         reservations[reservationId].tokenURI = _tokenURI;
         emit ReservationURIUpdated(_tokenURI, _msgSender(), reservationId);
     }
 
-    // Withdraw ETH from contract
-    function withdrawAll() public payable onlyOwner {
-        require(payable(msg.sender).send(address(this).balance));
+    function increaseShares(
+        uint256 tokenId,
+        uint256 shares,
+        address to
+    ) public {
+        require(shares > 0, "IP-NFT: shares amount must be greater than 0");
+        require(totalSupply(tokenId) == 1, "IP-NFT: shares already minted");
+        require(balanceOf(_msgSender(), tokenId) == 1, "IP-NFT: not owner");
+
+        _mint(to, tokenId, shares, "");
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
-        super._beforeTokenTransfer(from, to, tokenId);
+    // Withdraw ETH from contract
+    function withdrawAll() public payable onlyOwner {
+        require(
+            payable(msg.sender).send(address(this).balance),
+            "Not authorized"
+        );
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
-    function tokenURI(uint256 tokenId)
+    function uri(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        virtual
+        override(ERC1155, ERC1155URIStorage)
         returns (string memory)
     {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+        return super.uri(tokenId);
     }
 }
