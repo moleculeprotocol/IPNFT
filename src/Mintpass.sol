812ffcfd630885859e574b72632abebeb5f4349f
Dorian Wilhelm <wilhelm.dorian@pm.me>
2022-11-21 12:25:11 +0100

    implement changes related to comments on pr - makes _ipnftContract public - stores only revoked tokens in revokations mapping - removes numberOfValidTokens as its not really useful - shortens metadata - adds validity field to metadata - extends test setup with initial mint to bob

--- src/Mintpass.sol
+++ src/Mintpass.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { Base64 } from "@openzeppelin/contracts/utils/Base64.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Mintpass is ERC721, Ownable {
     using Counters for Counters.Counter;
@@ -12,13 +13,10 @@ contract Mintpass is ERC721, Ownable {
     Counters.Counter private _tokenIdCounter;
 
     /// @dev Stores the address of the associated IP-NFT contract.
-    address private _ipnftContract;
+    address public _ipnftContract;
 
-    // Mapping from tokenId to validity of token
-    mapping(uint256 => bool) private _isTokenValid;
-
-    // Mapping from owner to number of valid tokens
-    mapping(address => uint256) private _numberOfValidTokens;
+    // Mapping from tokenId to validity of token. If tokenId has been revoked, it will return true 
+    mapping(uint256 => bool) private _revokations;
 
     constructor(address ipnftContract_) ERC721("Mintpass", "MP") {
         _ipnftContract = ipnftContract_;
@@ -43,8 +41,12 @@ contract Mintpass is ERC721, Ownable {
     /// Event emitted when token is burned
     /// @param from Address that burned the token
     /// @param owner Address of the owner of the token before it got burned
-    /// @param tokenId Identifier of the token    
-    event TokenBurned(address indexed from, address indexed owner, uint256 indexed tokenId);
+    /// @param tokenId Identifier of the token
+    event TokenBurned(
+        address indexed from,
+        address indexed owner,
+        uint256 indexed tokenId
+    );
 
     /**
      *
@@ -56,15 +58,8 @@ contract Mintpass is ERC721, Ownable {
     /// @param tokenId Identifier of the token that is checked for validity
     /// @return True if the token is valid, false otherwise
     function isValid(uint256 tokenId) external view returns (bool) {
-        require(ownerOf(tokenId) != address(0), "Token does not exist");
-        return _isTokenValid[tokenId];
-    }
-
-    /// @dev Check if an address owns a valid token in the contract
-    /// @param owner Address for whom to check the ownership
-    /// @return True if `owner` has a valid token, false otherwise
-    function hasValid(address owner) public view virtual returns (bool) {
-        return _numberOfValidTokens[owner] > 0;
+        require(_exists(tokenId), "Token does not exist");
+        return !_revokations[tokenId];
     }
 
     /// @dev Mints a token to an address and approves it be handled by the IP-NFT Contract
@@ -72,8 +67,6 @@ contract Mintpass is ERC721, Ownable {
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _isTokenValid[tokenId] = true;
-        _numberOfValidTokens[to] += 1;
         _safeMint(to, tokenId);
         _approve(_ipnftContract, tokenId);
         emit TokenMinted(to, tokenId);
@@ -83,28 +76,18 @@ contract Mintpass is ERC721, Ownable {
     /// @param tokenId Identifier of the token
     function revoke(uint256 tokenId) external onlyOwner {
         address _owner = ownerOf(tokenId);
-        require(_isTokenValid[tokenId], "Token is already invalid");
-
-        assert(_numberOfValidTokens[_owner] > 0);
-        _numberOfValidTokens[_owner] -= 1;
-        _isTokenValid[tokenId] = false;
+        require(_revokations[tokenId] != true, "Token is already invalid");
+        _revokations[tokenId] = true;
         emit Revoked(_owner, tokenId);
     }
 
     /// @dev burns a token. This is only possible by either the owner of the token or the IP-NFT Contract
     /// @param tokenId Identifier of the token to be burned
     function burn(uint256 tokenId) external {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Not authorized to burn this token");
         address _owner = ownerOf(tokenId);
-        if (_owner == msg.sender || msg.sender == _ipnftContract) {
             _burn(tokenId);
             emit TokenBurned(msg.sender, _owner, tokenId);
-        } else {
-            revert("Only the owner or ipnft contract can burn this token.");
-        }
-    }
-
-    function numberOfValidTokens(address owner) public view returns (uint256) {
-        return _numberOfValidTokens[owner];
     }
 
     /// @dev Returns the tokenURI attached to a token
@@ -116,29 +99,20 @@ contract Mintpass is ERC721, Ownable {
         returns (string memory)
     {
         require(
-            ownerOf(tokenId) != address(0), "This token is not owned by anyone"
+            _exists(tokenId), "Token does not exist"
         );
-        return string(
-            abi.encodePacked(
-                "data:application/json;base64,",
-                Base64.encode(
-                    abi.encodePacked(
-                        '{"name": "Mintpass to create an IP-NFT", "description": "This Mintpass can be used to mint an IP-NFT. The Mintpass will get burned during the process", "external_url": "TODO: Enter IP-NFT-UI URL", "image":"TODO: Enter IPFS URL", "tokenId":"}',
-                        tokenId,
-                        '"}'
+        string memory valid = _revokations[tokenId] == true ? "Revoked" : "Valid";
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        abi.encodePacked(
+                            '{"name": "IP-NFT Mintpass #', Strings.toString(tokenId), '", "description": "This Mintpass can be used to mint one IP-NFT", "external_url": "TODO: Enter IP-NFT-UI URL", "image": "TODO: Enter IPFS URL", "valid": "', valid, '"}'
+                        )
                     )
                 )
-            )
-        );
-    }
-
-    // Returns the address to the associated IP-NFT Contract
-    function getAssociatedIPNFTContractAddress()
-        public
-        view
-        returns (address)
-    {
-        return _ipnftContract;
+            );
     }
 
     /**
--- test/Mintpass.t.sol
+++ test/Mintpass.t.sol
@@ -19,93 +19,68 @@ contract MintpassTest is Test {
     function setUp() public {
         vm.startPrank(deployer);
         token = new Mintpass(ipnftContract);
-        vm.stopPrank();
-    }
-
-    function testSafeMintFromOwner() public {
-        vm.startPrank(deployer);
         vm.expectEmit(true, true, true, true);
         emit TokenMinted(bob, 0);
         token.safeMint(bob);
-
+        
         assertEq(token.balanceOf(bob), 1);
-        assertEq(token.numberOfValidTokens(bob), 1);
         assertEq(token.isValid(0), true);
 
         string memory tokenUri_ = token.tokenURI(0);
         assertEq(
             tokenUri_,
-            "data:application/json;base64,eyJuYW1lIjogIk1pbnRwYXNzIHRvIGNyZWF0ZSBhbiBJUC1ORlQiLCAiZGVzY3JpcHRpb24iOiAiVGhpcyBNaW50cGFzcyBjYW4gYmUgdXNlZCB0byBtaW50IGFuIElQLU5GVC4gVGhlIE1pbnRwYXNzIHdpbGwgZ2V0IGJ1cm5lZCBkdXJpbmcgdGhlIHByb2Nlc3MiLCAiZXh0ZXJuYWxfdXJsIjogIlRPRE86IEVudGVyIElQLU5GVC1VSSBVUkwiLCAiaW1hZ2UiOiJUT0RPOiBFbnRlciBJUEZTIFVSTCIsICJ0b2tlbklkIjoifQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIn0="
+            "data:application/json;base64,eyJuYW1lIjogIklQLU5GVCBNaW50cGFzcyAjMCIsICJkZXNjcmlwdGlvbiI6ICJUaGlzIE1pbnRwYXNzIGNhbiBiZSB1c2VkIHRvIG1pbnQgb25lIElQLU5GVCIsICJleHRlcm5hbF91cmwiOiAiVE9ETzogRW50ZXIgSVAtTkZULVVJIFVSTCIsICJpbWFnZSI6ICJUT0RPOiBFbnRlciBJUEZTIFVSTCIsICJ2YWxpZCI6ICJWYWxpZCJ9"
         );
+
         vm.stopPrank();
     }
+
 
     function testSafeMintFromNotOwner() public {
         vm.startPrank(bob);
         vm.expectRevert("Ownable: caller is not the owner");
-        token.safeMint(bob);
-        assertEq(token.balanceOf(bob), 0);
+        token.safeMint(alice);
+
+        assertEq(token.balanceOf(alice), 0);
         vm.stopPrank();
     }
 
     function testTransfer() public {
-        vm.startPrank(deployer);
-        vm.expectEmit(true, true, true, true);
-        emit TokenMinted(bob, 0);
-        token.safeMint(bob);
-        vm.stopPrank();
-
         vm.startPrank(bob);
         vm.expectRevert("This a Soulbound token. It can only be burned.");
         token.transferFrom(bob, alice, 0);
+
         assertEq(token.balanceOf(bob), 1);
         vm.stopPrank();
     }
 
     function testBurnFromOwner() public {
-        vm.startPrank(deployer);
-        vm.expectEmit(true, true, true, true);
-        emit TokenMinted(bob, 0);
-        token.safeMint(bob);
-        assertEq(token.balanceOf(bob), 1);
-        vm.stopPrank();
-
+        vm.startPrank(bob);
         vm.expectEmit(true, true, true, true);
         emit TokenBurned(bob, bob, 0);
-        vm.startPrank(bob);
         token.burn(0);
+
         assertEq(token.balanceOf(bob), 0);
         vm.stopPrank();
     }
 
     function testBurnFromIpnftContract() public {
-        vm.startPrank(deployer);
-        vm.expectEmit(true, true, true, true);
-        emit TokenMinted(bob, 0);
-        token.safeMint(bob);
-        assertEq(token.balanceOf(bob), 1);
-        vm.stopPrank();
-
         vm.startPrank(ipnftContract);
+        assertEq(token.ownerOf(0), bob);
+
         vm.expectEmit(true, true, true, true);
         emit TokenBurned(ipnftContract, bob, 0);
-        assertEq(token.ownerOf(0), bob);
         token.burn(0);
+
         assertEq(token.balanceOf(bob), 0);
         vm.stopPrank();
     }
 
     function testBurnFromRandomUser() public {
-        vm.startPrank(deployer);
-        vm.expectEmit(true, true, true, true);
-        emit TokenMinted(bob, 0);
-        token.safeMint(bob);
-        assertEq(token.balanceOf(bob), 1);
-        vm.stopPrank();
-
         vm.startPrank(alice);
-        vm.expectRevert("Only the owner or ipnft contract can burn this token.");
+        vm.expectRevert("Not authorized to burn this token");
         token.burn(0);
+
         assertEq(token.balanceOf(bob), 1);
         vm.stopPrank();
     }
@@ -113,30 +88,24 @@ contract MintpassTest is Test {
     function testRevokeToken() public {
         vm.startPrank(deployer);
         vm.expectEmit(true, true, true, true);
-        emit TokenMinted(bob, 0);
-        token.safeMint(bob);
-        assertEq(token.balanceOf(bob), 1);
-        assertEq(token.isValid(0), true);
-
-        vm.expectEmit(true, true, true, true);
         emit Revoked(bob, 0);
         token.revoke(0);
-        // bool isValid = token.isValid(0);
+
         assertEq(token.isValid(0), false);
+
+        string memory tokenUri_ = token.tokenURI(0);
+        assertEq(
+            tokenUri_,
+            "data:application/json;base64,eyJuYW1lIjogIklQLU5GVCBNaW50cGFzcyAjMCIsICJkZXNjcmlwdGlvbiI6ICJUaGlzIE1pbnRwYXNzIGNhbiBiZSB1c2VkIHRvIG1pbnQgb25lIElQLU5GVCIsICJleHRlcm5hbF91cmwiOiAiVE9ETzogRW50ZXIgSVAtTkZULVVJIFVSTCIsICJpbWFnZSI6ICJUT0RPOiBFbnRlciBJUEZTIFVSTCIsICJ2YWxpZCI6ICJSZXZva2VkIn0="
+        );
         vm.stopPrank();
     }
 
     function testRevokeFromRandomUser() public {
-        vm.startPrank(deployer);
-        vm.expectEmit(true, true, true, true);
-        emit TokenMinted(bob, 0);
-        token.safeMint(bob);
-        assertEq(token.balanceOf(bob), 1);
-        vm.stopPrank();
-
         vm.startPrank(bob);
         vm.expectRevert("Ownable: caller is not the owner");
         token.revoke(0);
+
         bool isValid = token.isValid(0);
         assertEq(isValid, true);
         vm.stopPrank();
