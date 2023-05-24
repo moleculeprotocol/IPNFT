90bdce545ee5d310c0d355f4643369f1b884506b
Stefan Adolf <stefan@molecule.to>
2023-05-25 00:20:08 +0200

    fix: handleFailed also saves the state applies new prettier config upgrades graphcli to 0.50.1
    Signed-off-by: Stefan Adolf <stefan@molecule.to>

--- subgraph/src/stakedVestedCrowdSaleMapping.ts
+++ subgraph/src/stakedVestedCrowdSaleMapping.ts
@@ -1,5 +1,5 @@
-import { BigInt, log } from '@graphprotocol/graph-ts';
-import { IERC20Metadata } from '../generated/StakedVestedCrowdSale/IERC20Metadata';
+import { BigInt, log } from '@graphprotocol/graph-ts'
+import { IERC20Metadata } from '../generated/StakedVestedCrowdSale/IERC20Metadata'
 import {
   Bid as BidEvent,
   Staked as StakedEvent,
@@ -7,151 +7,152 @@ import {
   Started as StartedEvent,
   Failed as FailedEvent,
   Claimed as ClaimedEvent
-} from '../generated/StakedVestedCrowdSale/StakedVestedCrowdSale';
+} from '../generated/StakedVestedCrowdSale/StakedVestedCrowdSale'
 
 import {
   Contribution,
   CrowdSale,
   ERC20Token,
   Fractionalized
-} from '../generated/schema';
+} from '../generated/schema'
 
 function makeERC20Token(_contract: IERC20Metadata): ERC20Token {
-  let token = ERC20Token.load(_contract._address);
+  let token = ERC20Token.load(_contract._address)
 
   if (!token) {
-    token = new ERC20Token(_contract._address);
-    token.id = _contract._address;
-    token.decimals = BigInt.fromI32(_contract.decimals());
-    token.symbol = _contract.symbol();
-    token.name = _contract.name();
+    token = new ERC20Token(_contract._address)
+    token.id = _contract._address
+    token.decimals = BigInt.fromI32(_contract.decimals())
+    token.symbol = _contract.symbol()
+    token.name = _contract.name()
   }
 
-  token.save();
-  return token;
+  token.save()
+  return token
 }
 
 export function handleStarted(event: StartedEvent): void {
-  let crowdSale = new CrowdSale(event.params.saleId.toString());
+  let crowdSale = new CrowdSale(event.params.saleId.toString())
 
   let fractionalized = Fractionalized.load(
     event.params.sale.auctionToken.toHexString()
-  );
+  )
   if (!fractionalized) {
     log.error('Fractionalized Ipnft not found for id: {}', [
       event.params.sale.auctionToken.toHexString()
-    ]);
-    return;
+    ])
+    return
   }
-  crowdSale.fractionalizedIpnft = fractionalized.id;
-  crowdSale.issuer = event.params.issuer;
-  crowdSale.beneficiary = event.params.sale.beneficiary;
-  crowdSale.closingTime = event.params.sale.closingTime;
-  crowdSale.createdAt = event.block.timestamp;
-  crowdSale.state = 'RUNNING';
+  crowdSale.fractionalizedIpnft = fractionalized.id
+  crowdSale.issuer = event.params.issuer
+  crowdSale.beneficiary = event.params.sale.beneficiary
+  crowdSale.closingTime = event.params.sale.closingTime
+  crowdSale.createdAt = event.block.timestamp
+  crowdSale.state = 'RUNNING'
 
   crowdSale.auctionToken = makeERC20Token(
     IERC20Metadata.bind(event.params.sale.auctionToken)
-  ).id;
-  crowdSale.salesAmount = event.params.sale.salesAmount;
+  ).id
+  crowdSale.salesAmount = event.params.sale.salesAmount
   crowdSale.vestedAuctionToken = makeERC20Token(
     IERC20Metadata.bind(event.params.vesting.vestingContract)
-  ).id;
+  ).id
 
-  crowdSale.auctionCliff = event.params.vesting.cliff;
+  crowdSale.auctionCliff = event.params.vesting.cliff
 
   crowdSale.biddingToken = makeERC20Token(
     IERC20Metadata.bind(event.params.sale.biddingToken)
-  ).id;
-  crowdSale.fundingGoal = event.params.sale.fundingGoal;
-  crowdSale.amountRaised = BigInt.fromU32(0);
+  ).id
+  crowdSale.fundingGoal = event.params.sale.fundingGoal
+  crowdSale.amountRaised = BigInt.fromU32(0)
   crowdSale.stakingToken = makeERC20Token(
     IERC20Metadata.bind(event.params.staking.stakedToken)
-  ).id;
+  ).id
 
-  crowdSale.amountStaked = BigInt.fromU32(0);
+  crowdSale.amountStaked = BigInt.fromU32(0)
   crowdSale.vestedStakingToken = makeERC20Token(
     IERC20Metadata.bind(event.params.staking.stakesVestingContract)
-  ).id;
-  crowdSale.stakingCliff = event.params.vesting.cliff;
-  crowdSale.save();
+  ).id
+  crowdSale.stakingCliff = event.params.vesting.cliff
+  crowdSale.save()
 }
 
 export function handleBid(event: BidEvent): void {
-  let crowdSale = CrowdSale.load(event.params.saleId.toString());
+  let crowdSale = CrowdSale.load(event.params.saleId.toString())
   if (!crowdSale) {
     log.error('CrowdSale not found for id: {}', [
       event.params.saleId.toString()
-    ]);
-    return;
+    ])
+    return
   }
 
   //   Update CrowdSale
-  crowdSale.amountRaised = crowdSale.amountRaised.plus(event.params.amount);
+  crowdSale.amountRaised = crowdSale.amountRaised.plus(event.params.amount)
 
-  crowdSale.save();
+  crowdSale.save()
 
   //   Create Contribution
-  let contribution = new Contribution(event.transaction.hash.toHexString());
-  contribution.amount = event.params.amount;
-  contribution.contributor = event.params.bidder;
-  contribution.createdAt = event.block.timestamp;
-  contribution.crowdSale = crowdSale.id;
+  let contribution = new Contribution(event.transaction.hash.toHexString())
+  contribution.amount = event.params.amount
+  contribution.contributor = event.params.bidder
+  contribution.createdAt = event.block.timestamp
+  contribution.crowdSale = crowdSale.id
 
-  contribution.save();
+  contribution.save()
 }
 
 export function handleStaked(event: StakedEvent): void {
-  let crowdSale = CrowdSale.load(event.params.saleId.toString());
+  let crowdSale = CrowdSale.load(event.params.saleId.toString())
   if (!crowdSale) {
     log.error('CrowdSale not found for id: {}', [
       event.params.saleId.toString()
-    ]);
-    return;
+    ])
+    return
   }
   crowdSale.amountStaked = crowdSale.amountStaked.plus(
     event.params.stakedAmount
-  );
+  )
 
-  let contribution = Contribution.load(event.transaction.hash.toHexString());
+  let contribution = Contribution.load(event.transaction.hash.toHexString())
   if (!contribution) {
     log.error('cannot associate contribution for stake handler {}', [
       event.transaction.hash.toHexString()
-    ]);
-    return;
+    ])
+    return
   }
-  contribution.stakedAmount = event.params.stakedAmount;
-  contribution.price = event.params.price;
-  contribution.save();
+  contribution.stakedAmount = event.params.stakedAmount
+  contribution.price = event.params.price
+  contribution.save()
 }
 
 export function handleSettled(event: SettledEvent): void {
-  let crowdSale = CrowdSale.load(event.params.saleId.toString());
+  let crowdSale = CrowdSale.load(event.params.saleId.toString())
   if (!crowdSale) {
     return log.error('CrowdSale not found for id: {}', [
       event.params.saleId.toString()
-    ]);
+    ])
   }
-  crowdSale.state = 'SETTLED';
-  crowdSale.save();
+  crowdSale.state = 'SETTLED'
+  crowdSale.save()
 }
 
 export function handleFailed(event: FailedEvent): void {
-  let crowdSale = CrowdSale.load(event.params.saleId.toString());
+  let crowdSale = CrowdSale.load(event.params.saleId.toString())
   if (!crowdSale) {
     return log.error('CrowdSale not found for id: {}', [
       event.params.saleId.toString()
-    ]);
+    ])
   }
-  crowdSale.state = 'FAILED';
+  crowdSale.state = 'FAILED'
+  crowdSale.save()
 }
 
 export function handleClaimed(event: ClaimedEvent): void {
-  let crowdSale = CrowdSale.load(event.params.saleId.toString());
+  let crowdSale = CrowdSale.load(event.params.saleId.toString())
   if (!crowdSale) {
     log.error('CrowdSale not found for id: {}', [
       event.params.saleId.toString()
-    ]);
-    return;
+    ])
+    return
   }
 }
