e383d9cd41de897cb58090edf7e609b4146905f9
Stefan Adolf <stefan@molecule.to>
2023-07-21 14:21:07 +0200

    Dune Query Updates (#140)
    * reorders tenderly files
    versions dune sql queries
    
    Signed-off-by: Stefan Adolf <stefan@molecule.to>
    
    * drops ipnft prefix
    updates cumulative bids query
    
    Signed-off-by: Stefan Adolf <stefan@molecule.to>
    
    * uses generic prices table to fetch decimals
    reduces crowdsale bids query
    
    Signed-off-by: Stefan Adolf <stefan@molecule.to>
    
    * falls back to generic tokens.erc20 spell
    
    Signed-off-by: stadolf <stefan@molecule.to>
    
    ---------
    
    Signed-off-by: Stefan Adolf <stefan@molecule.to>
    Signed-off-by: stadolf <stefan@molecule.to>

---
+++ periphery/dune/cumulative_bids.sql
@@ -0,0 +1,15 @@
+SELECT
+  bids.evt_block_time,
+  SUM(previous_bids.amount) / 1e18 AS cumulative_growth
+FROM 
+  ipnft_{{chain}}.StakedLockingCrowdSale_evt_Bid as bids
+LEFT JOIN ipnft_{{chain}}.StakedLockingCrowdSale_evt_Bid 
+  AS previous_bids 
+  ON previous_bids.evt_block_time <= bids.evt_block_time
+WHERE
+  bids.saleId = cast('{{saleId}}' as uint256)
+GROUP BY
+  bids.evt_block_time,
+  bids.amount
+ORDER BY
+  bids.evt_block_time;
