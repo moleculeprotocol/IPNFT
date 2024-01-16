b4040ac2311843f485f5a3e8a90014bc9638640e
stadolf <stefan@molecule.to>
2024-01-16 10:31:44 +0100

    renames rollout script updates new addresses
    Signed-off-by: stadolf <stefan@molecule.to>

--- script/prod/RolloutTokenizerV12.s.sol
+++ script/prod/RolloutTokenizerV12.s.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import "forge-std/Script.sol";
-import {Tokenizer} from '../../src/Tokenizer.sol';
+import { Tokenizer } from "../../src/Tokenizer.sol";
 import { IPToken } from "../../src/IPToken.sol";
 import { console } from "forge-std/console.sol";
 
-contract UpgradeTokenizer11_12 is Script {
-
+contract RolloutTokenizerV12 is Script {
     function run() public {
         vm.startBroadcast();
 
@@ -14,11 +14,11 @@ contract UpgradeTokenizer11_12 is Script {
         Tokenizer newTokenizerImplementation = new Tokenizer();
 
         bytes memory upgradeCallData = abi.encodeWithSelector(Tokenizer.setIPTokenImplementation.selector, address(newIpTokenImplementation));
-        
+
         console.log("NEWTOKENIMPLEMENTATION=%s", address(newIpTokenImplementation));
         console.log("NEWTOKENIZER=%s", address(newTokenizerImplementation));
         console.logBytes(upgradeCallData);
-       
+
         vm.stopBroadcast();
     }
 }
