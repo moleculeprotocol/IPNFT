0b787001624200731768937dd2e9c60172cfda91
stadolf <stadolf@gmail.com>
2024-02-24 12:02:59 -0700

    drops crowdsale unrelated contracts
    Signed-off-by: stadolf <stadolf@gmail.com>
    
    drops some older dependencies
    
    Signed-off-by: stadolf <stadolf@gmail.com>
    
    forge install: openzeppelin-contracts
    
    v5.0.1
    
    drops crowdsale unrelated mappings
    
    Signed-off-by: stadolf <stadolf@gmail.com>

--- subgraph/makeAbis.sh
+++ subgraph/makeAbis.sh
@@ -1,76 +1,10 @@
 #!/usr/bin/env bash
 
 cat ../out/IERC20Metadata.sol/IERC20Metadata.json | jq .abi > abis/IERC20Metadata.json
-cat ../out/IPNFT.sol/IPNFT.json | jq .abi > ./abis/IPNFT.json
-cat ../out/Mintpass.sol/Mintpass.json | jq .abi > ./abis/Mintpass.json
-cat ../out/SchmackoSwap.sol/SchmackoSwap.json | jq .abi > ./abis/SchmackoSwap.json
-cat ../out/IPToken.sol/IPToken.json | jq .abi > ./abis/IPToken.json
 cat ../out/TimelockedToken.sol/TimelockedToken.json | jq .abi > ./abis/TimelockedToken.json
-cat ../out/SalesShareDistributor.sol/SalesShareDistributor.json | jq .abi > ./abis/SharedSalesDistributor.json
 cat ../out/Permissioner.sol/TermsAcceptedPermissioner.json | jq .abi > ./abis/TermsAcceptedPermissioner.json
 cat ../out/StakedLockingCrowdSale.sol/StakedLockingCrowdSale.json | jq .abi > ./abis/StakedLockingCrowdSale.json
 cat ../out/CrowdSale.sol/CrowdSale.json | jq .abi > ./abis/CrowdSale.json
-
-cat ../out/Tokenizer.sol/Tokenizer.json | jq .abi > ./abis/_Tokenizer.json
-
-# add the old Synthesizer's `MoleculesCreated` event to the Tokenizer abi so the subgraph can index them
-jq '. +=  [{
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "moleculesId",
-        "type": "uint256"
-      },
-      {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "ipnftId",
-        "type": "uint256"
-      },
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "tokenContract",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "emitter",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "string",
-        "name": "agreementCid",
-        "type": "string"
-      },
-      {
-        "indexed": false,
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
-      },
-      {
-        "indexed": false,
-        "internalType": "string",
-        "name": "symbol",
-        "type": "string"
-      }
-    ],
-    "name": "MoleculesCreated",
-    "type": "event"
-  }]' ./abis/_Tokenizer.json > ./abis/Tokenizer.json
-
-  rm ./abis/_Tokenizer.json
 
 # add the old StakedLockingCrowdSale's `Started` event to the abi so the subgraph can index them
 #event Started(uint256 indexed saleId, address indexed issuer, Sale sale);
