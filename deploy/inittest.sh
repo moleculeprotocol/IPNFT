78638e5bd4d78498245ae1976fe2da3be31ab72f
stadolf <stefan@molecule.to>
2024-07-08 12:10:20 +0200

    updates subgraph dependencies & config uses latest foundry image uses latest graph specs replaces graph templating with network feature adds network string replacements mainnet
    Signed-off-by: stadolf <stefan@molecule.to>

--- deploy/inittest.sh
+++ deploy/inittest.sh
@@ -14,10 +14,14 @@ $DC ps
 ./setupLocal.sh -f
 
 cd subgraph
-yarn prepare:local
 yarn codegen
+yarn build:local
 yarn create:local
+# that's a bad local config hack still required: the root's subgraph.yaml's network must be "mainnet" for foundry
+sed -i '' -e 's/network\: foundry/network\: mainnet/g' build/subgraph.yaml
+sed -i '' -e 's/network\: foundry/network\: mainnet/g' subgraph.yaml
 yarn deploy:local -l v0.0.1
+sed -i '' -e 's/network\: mainnet/network\: foundry/g' subgraph.yaml
 cd ..
 
 $DC exec -T postgres pg_dump -Fc -U graph-node -w graph-node -f after_setup.dump
