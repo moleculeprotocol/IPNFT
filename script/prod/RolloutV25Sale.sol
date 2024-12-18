d9a410257c81b3b90fc7bcbb5607495e2c84e703
Stefan Adolf <stefan@molecule.to>
2024-12-18 19:54:16 +0100

    rolls out base locking crowdsales
    Signed-off-by: Stefan Adolf <stefan@molecule.to>

--- script/prod/RolloutV25Sale.sol
+++ script/prod/RolloutV25Sale.sol
@@ -7,6 +7,7 @@ import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy
 import { IPNFT } from "../../src/IPNFT.sol";
 import { IPermissioner, TermsAcceptedPermissioner } from "../../src/Permissioner.sol";
 import { StakedLockingCrowdSale } from "../../src/crowdsale/StakedLockingCrowdSale.sol";
+import { LockingCrowdSale } from "../../src/crowdsale/LockingCrowdSale.sol";
 import { TimelockedToken } from "../../src/TimelockedToken.sol";
 
 contract RolloutV25Sale is Script {
@@ -21,6 +22,25 @@ contract RolloutV25Sale is Script {
 
         console.log("STAKED_LOCKING_CROWDSALE_ADDRESS=%s", address(stakedLockingCrowdSale));
         console.log("timelocked token implementation=%s", address(timelockedTokenImplementation));
+        // 0x7c36c64DA1c3a2065074caa9C48e7648FB733aAB
+        // vestedDaoToken.grantRole(vestedDaoToken.ROLE_CREATE_SCHEDULE(), address(stakedLockingCrowdSale));
+        // stakedLockingCrowdSale.trustVestingContract(vestedDaoToken);
+    }
+}
+
+contract RolloutV25LockingSale is Script {
+    function run() public {
+        //mainnet 0xCfA0F84660fB33bFd07C369E5491Ab02C449f71B;
+        address moleculeDevMultisig = 0x9d5a6ae551f1117946FF6e0e86ef9A1B20C90Cb0;
+        TimelockedToken timelockedTokenImplementation = TimelockedToken(0xF8F79c1E02387b0Fc9DE0945cD9A2c06F127D851); 
+
+        vm.startBroadcast();
+        LockingCrowdSale lockingCrowdsale = new LockingCrowdSale(timelockedTokenImplementation);
+        lockingCrowdsale.transferOwnership(moleculeDevMultisig);
+        vm.stopBroadcast();
+
+        console.log("LOCKING_CROWDSALE_ADDRESS=%s", address(lockingCrowdsale));
+        //console.log("timelocked token implementation=%s", address(timelockedTokenImplementation));
         // 0x7c36c64DA1c3a2065074caa9C48e7648FB733aAB
         // vestedDaoToken.grantRole(vestedDaoToken.ROLE_CREATE_SCHEDULE(), address(stakedLockingCrowdSale));
         // stakedLockingCrowdSale.trustVestingContract(vestedDaoToken);
